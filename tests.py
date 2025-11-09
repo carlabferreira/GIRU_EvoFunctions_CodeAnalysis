8a89013208570e4dd2b62ceb8b307855cbb7c7bc
carlabferreira <carla.be.ferreira@gmail.com>
2025-11-08 22:02:35 -0300

    Add GitHub Actions workflow for testing with pytest and create requirements.txt

---
+++ tests.py
@@ -0,0 +1,11 @@
+from main import display_limits, filterForPythonFiles
+
+def test_invalid_token():
+    invalid_token = "invalid_token"
+    #todo chamar parte da main com token invalido e verificar se o erro é tratado
+    assert True
+
+def test_invalid_repo():
+    invalid_repo = "invalid/repo"
+    #todo chamar parte da main com repositorio invalido e verificar se o erro é tratado
+    assert True
